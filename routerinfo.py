afddf0993b1d242006e2ebebd817c11787416b9a
Vaibhav Arora <72528539+varora24@users.noreply.github.com>
2021-06-02 02:12:03 +0530

    Update routerinfo.py

--- routerinfo.py
+++ routerinfo.py
@@ -2,7 +2,7 @@ import pprint
 import datetime
 from pynetgear import Netgear
 
-netgear = Netgear(password="Arora@2003")
+netgear = Netgear(password="*******")
 devices = netgear.get_attached_devices()
 #for i in devices:
 #    pprint.pprint(i[0])
